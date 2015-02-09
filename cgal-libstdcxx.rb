fc6ebc118ca4d07379be0315d517605e55ebfc3e
Neill D.F. Campbell <neill.campbell@cantab.net>
2015-02-09 18:59:10 +0000

    Added cgal in stdcxx form.

--- cgal-libstdcxx.rb
+++ cgal-libstdcxx.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Cgal < Formula
+class CgalLibstdcxx < Formula
   homepage 'http://www.cgal.org/'
   url 'https://gforge.inria.fr/frs/download.php/34149/CGAL-4.5.tar.gz'
   sha1 'd505d4257f214b200949d67570ad743d3a913633'
