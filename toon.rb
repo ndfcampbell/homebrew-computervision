d12cfbe9233544d5bb281479364fd3751a8b9f27
Neill D.F. Campbell <neill.campbell@cantab.net>
2019-02-15 15:47:31 +0000

    Toon on github.

--- toon.rb
+++ toon.rb
@@ -5,7 +5,8 @@ require 'formula'
 
 class Toon < Formula
   homepage 'http://www.edwardrosten.com/cvd/toon.html'
-  url 'http://git.savannah.gnu.org/cgit/toon.git'
+  #url 'http://git.savannah.gnu.org/cgit/toon.git'
+  url 'git://github.com/edrosten/TooN.git'
   version '2.0'
 
   # depends_on 'cmake' => :build
